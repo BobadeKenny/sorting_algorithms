9c7316512c84cb9bce2a456370a1557e59296475
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-07-15 23:26:16 +0100

    betty corrections for 0 and 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,30 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order using the Bubble sort algorithm
+ * bubble_sort - sorts an array of integers
+ *in ascending order using the Bubble sort algorithm
  * @array: array to be sorted
  * @size: size of the array
  * Return: Null
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;
-    int temp;
-    int swapped;
+size_t i;
+int temp;
+int swapped;
 
-    do {
-        swapped = 0;
-        for (i=1; i<size; i++)
-        {
-            if (array[i-1] > array[i])
-            {
-                temp = array[i-1];
-                array[i-1] = array[i];
-                array[i] = temp;
-                print_array(array, size);
-                swapped = 1;
-            }
-        }
-    } while(swapped);
+do {
+swapped = 0;
+for (i = 1; i < size; i++)
+{
+if (array[i - 1] > array[i])
+{
+temp = array[i - 1];
+array[i - 1] = array[i];
+array[i] = temp;
+print_array(array, size);
+swapped = 1;
+}
+}
+} while (swapped);
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers in ascending order using the Bubble sort algorithm
+ * selection_sort - sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
  * @array: array to be sorted
  * @size: size of the array
  * Return: Null
@@ -9,27 +10,27 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i;
-    size_t j;
-    size_t min;
-    int temp;
+size_t i;
+size_t j;
+size_t min;
+int temp;
 
-    for (i=0; i<size; i++)
-    {
-        min = i;
-        for (j=i+1; j<size; j++)
-        {
-            if (array[j]<array[min])
-            {
-                min = j;
-            }
-        }
-        if (min != i)
-        {
-            temp = array[min];
-            array[min] = array[i];
-            array[i] = temp;
-            print_array(array, size);
-        }
-    }
+for (i = 0; i < size; i++)
+{
+min = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min])
+{
+min = j;
+}
+}
+if (min != i)
+{
+temp = array[min];
+array[min] = array[i];
+array[i] = temp;
+print_array(array, size);
+}
+}
 }
