d31a67ebbc8fa222ee5e6a80de3a5720d836f9cd
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-07-18 07:54:43 +0100

    quick sort done

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,37 +2,37 @@
 #include <stdio.h>
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order
  * @list: list to be sorted
  * Return: Null
  */
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *temp;
-    listint_t *node;
+listint_t *node;
+listint_t *temp;
 
-    if (*list == NULL)
-    {}
-    node = (*list)->next;
-    while (node != NULL)
-    {
-        temp = node;
-        while (temp->prev != NULL && temp->prev->n > temp->n)
-        {
-            temp->prev->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-			temp->next = temp->prev;
-			temp->prev = temp->next->prev;
-			temp->next->prev = temp;
-			if (temp->prev == NULL)
-				*list = temp;
-			else
-				temp->prev->next = temp;
-			print_list(*list);
-        }
-        node = node->next;
-
-    }
+if (*list == NULL)
+{}
+node = (*list)->next;
+while (node != NULL)
+{
+temp = node;
+while (temp->prev != NULL && temp->prev->n > temp->n)
+{
+temp->prev->next = temp->next;
+if (temp->next != NULL)
+temp->next->prev = temp->prev;
+temp->next = temp->prev;
+temp->prev = temp->next->prev;
+temp->next->prev = temp;
+if (temp->prev == NULL)
+*list = temp;
+else
+temp->prev->next = temp;
+print_list(*list);
+}
+node = node->next;
+}
 }
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,52 +1,69 @@
 #include "sort.h"
 
 /**
- * quick_sort - sorts an array of integers in ascending order using the Bubble sort algorithm
+ * quick_sort - sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
  * @array: array to be sorted
  * @size: size of the array
  * Return: Null
  */
 void quick_sort(int *array, size_t size)
 {
-size_t high, low;
 
-high = size - 1;
-low = 0;
-quicksort(array, low, high, size);
+quicksort(array, 0, size - 1, size);
 }
 
-void quicksort(int *array, size_t low, size_t high, size_t size)
+/**
+ * quicksort - sorts an array of integers
+ * in ascending order using the Lomuto partition
+ * @array: array to be sorted
+ * @low: start index
+ * @high: ending index
+ * @size: size of the array
+ * Return: Null
+ */
+void quicksort(int *array, int low, int high, size_t size)
 {
 int p;
 
-if (low >= high || low < 0.0)
-{}
+if (low < high)
+{
 p = partition(array, low, high, size);
-quicksort(array, low, p-1, size);
-quicksort(array, p+1, high, size);
+quicksort(array, low, p - 1, size);
+quicksort(array, p + 1, high, size);
+}
 }
 
-int partition(int *array, size_t low, size_t high, size_t size)
+/**
+ * partition - sorts an array of integers
+ * in ascending order using the Lomuto partition
+ * @array: array to be sorted
+ * @low: start index
+ * @high: ending index
+ * @size: size of the array
+ * Return: Null
+ */
+int partition(int *array, int low, int high, size_t size)
 {
 int pivot, temp;
-size_t i, j;
+int i, j;
 
 pivot = array[high];
 i = low - 1;
-for(j  = low; j < high; j++)
+for (j  = low; j < high; j++)
 {
-if (array[j] <= pivot)
+if (array[j] < pivot)
 {
-i += 1;
-}
+i++;
 temp = array[i];
 array[i] = array[j];
 array[j] = temp;
+}
+}
 i += 1;
 temp = array[i];
 array[i] = array[high];
 array[high] = temp;
 print_array(array, size);
-}
 return (i);
 }
