e934efdaee96a90f0bae488e3c47cc74c721de70
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-07-19 13:32:01 +0100

    counting sort done

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -8,5 +8,30 @@
  */
 void cocktail_sort_list(listint_t **list)
 {
+int swapped;
+listint_t temp;
+listint_t node;
 
+if (*list == NULL)
+{}
+temp = *list;
+do {
+swapped = 0;
+while (temp != NULL && temp->next != NULL)
+{
+if (temp->n > temp->next->n)
+{
+temp->prev = temp->next;
+temp->next = temp->next->next;
+temp->next->prev = temp->prev;
+temp->next->next = temp->next->prev
+swapped = 1;
+if (temp->prev == NULL)
+*list = temp;
+else
+temp->prev->next = temp;
+print_list(*list);
+}
+}
+} while (swapped);
 }
---
+++ 102-counting_sort.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+#include <stdlib.h>
+
+/**
+ * counting_sort - sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: Null
+ */
+
+void counting_sort(int *array, size_t size)
+{
+int i,k;
+size_t j;
+int *count;
+int *output;
+
+output = malloc(sizeof(int) * size);
+k = array[0];
+for (j = 1; j < size; j++)
+{
+if (array[j] > k)
+{
+k = array[j];
+}
+}
+count = malloc(sizeof(int) * (k + 1));
+for (i = 0; i <= k; i++)
+{
+count[i] = 0;
+}
+for (j = 0; j < size; j++)
+{
+count[array[j]] += 1;
+}
+for (i = 1; i <= k; i++)
+{
+count[i] += count[i - 1];
+}
+print_array(count, k + 1);
+for (i = size - 1; i >= 0; i--)
+{
+count[array[i]] -= 1;
+output[count[array[i]]] = array[i];
+}
+for (j = 0; j < size; j++)
+{
+array[j] = output[j];
+}
+free(count);
+free(output);
+}
